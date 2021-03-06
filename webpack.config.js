6ae211477e5a21087c5b97b983fdf5e957466c28
Kosanyak19 <kosvik264@gmail.com>
2021-03-06 22:16:27 +0200

    fail

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, argv) => {
         module: {
             rules: [
                 {
-                    test: /.jsx?$/,
+                    test: /.js?$/,
                     use: ['babel-loader'],
                 },
                 {
